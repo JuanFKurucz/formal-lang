5d1acb2be91e86a0a65fcfc10fc08a0f2ba7673b
bcattaneo <c@ttaneo.uy>
2021-05-29 20:11:42 -0300

    Agrego funciones faltantes de math

--- UT3ej2.4/mathp.js
+++ UT3ej2.4/mathp.js
@@ -3,29 +3,36 @@ const grammar = require("./math.js");
 const lexer = require('./lexer.js');
 const prompt = require('prompt-sync')({ sigint: true });
 
-
-// TODO: los argumentos del mathp.js(-p y el: t)
-
-// TODO: Cambiar a https://preview.npmjs.com/package/minimist
-const args = process.argv.slice(2);
+const argv = require('minimist')(process.argv.slice(2));
 
 // Toggle whether to parse (evaluate)
 // or just print all the expression tokens
-const parse = args[0] != '-t';
+const tokenize = argv["t"];
+const expression = argv["e"];
 
-while (true) {
-    const expr = prompt('> ');
-    const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
+let parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
 
-    if (parse) {
-        // Evaluate
-        parser.feed(expr);
-        console.log(parser.results[0]);
-    } else {
-        // Print tokens
-        lexer.reset(expr)
-        for (const token of lexer) {
-            console.log(`${token.type}: ${JSON.stringify(token.text)}`);
+if (expression && typeof(expression) !== "boolean") {
+    // Evaluate expression and quit
+    parser.feed(expression.toString());
+    console.log(parser.results[0]);
+} else {
+    // Prompt the user
+    while (true) {
+        let expr = prompt('> ');
+        parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
+
+        if (tokenize || expr.startsWith(":t ")) {
+            // Print tokens
+            expr = expr.replace(":t ", "");
+            lexer.reset(expr)
+            for (const token of lexer) {
+                console.log(`${token.type}: ${JSON.stringify(token.text)}`);
+            }
+        } else {
+            // Evaluate
+            parser.feed(expr);
+            console.log(parser.results[0]);
         }
     }
 }
